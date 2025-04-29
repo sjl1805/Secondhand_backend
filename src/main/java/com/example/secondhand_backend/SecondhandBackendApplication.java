f89c57f48a2040c64e5fa127e70199719dcf78fe
qiugongming <2861903619@qq.com>
2025-04-29 13:23:44 +0800

    01

---
+++ src/main/java/com/example/secondhand_backend/SecondhandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondhand_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondhandBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secondhand_backend/SecondhandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondhand_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
